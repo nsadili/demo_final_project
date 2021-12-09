fa0612b4167ffee2730c2430687ea0077bc5eb0d
Nuraddin Sh. Sadili <nsadili@ada.edu.az>
2021-12-09 14:32:54 +0400

    Added team 00 project.

---
+++ Team_00/Main.java
@@ -0,0 +1,7 @@
+package Team_00;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Here you will work on your final project...");
+    }
+}
--- test folder/Main.java
+++ test folder/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("We are the champions");
+        System.out.println("Believe me you are...");
 
     }
 
